a4249e98d688d9b648f41b3a0990d9391ade8420
Nick Justin <Nicholasgjustin@gmail.com>
2018-12-05 13:33:11 -0600

    Completed Drill 4, added four objects, put them into an array, and created for loop calling object names and jobTitles

--- gistDrills.js
+++ gistDrills.js
@@ -27,7 +27,36 @@ for(const key in drillObject){
 	
 }
 
+//Drill 3
 let drillObject2 = {
 	meals: ['breakfast', 'second breakfast', 'elevenses', 'lunch', 'afternoon tea', 'dinner', 'supper'],
 }
 console.log(drillObject2.meals[3]);
+
+//Drill 4
+
+let object1 = {
+  name: 'David Johnson',
+  jobTitle: 'Master of Arguing In A Classy Manner'
+};
+
+let object2 = {
+  name: 'Nick Justin',
+  jobTitle: 'Swift Wit Regarding Languages'
+};
+
+let object3 = {
+  name: 'Santa Claus',
+  jobTitle: 'Santa'
+};
+
+let object4 = {
+  name: 'Comrade Ducky-san',
+  jobTitle: 'Rubber Ducky Stand-In'
+};
+
+let objectArray = [object1, object2, object3, object4];
+
+for(x = 0; x < objectArray.length; x++){
+  console.log(`${objectArray[x].name} is a great ${objectArray[x].jobTitle}`)
+}
