9975924eb0094e856c07537a013b3c4beb151f7f
Nick Justin <Nicholasgjustin@gmail.com>
2018-12-05 15:49:19 -0600

    Added Variable Heroes, added two functions, neither complete.

--- gistDrills.js
+++ gistDrills.js
@@ -104,6 +104,7 @@ function decodeWords(stringOfWords){
 
 console.log(decodeWords(randomWords));
 */
+/*
 function createCharacter(Name, nickName, race, origin, attack, defense) {
   return {
     name: Name,
@@ -140,3 +141,23 @@ console.log(hobbitArray);
 let strongCharArray = characterArray.filter(attack => attack.attack > 5);
 console.log(strongCharArray);
 //long discusion was had about the weapon thing.
+*/
+
+const HEROES = [
+  { id: 1, name: 'Captain America', squad: 'Avengers' },
+  { id: 2, name: 'Iron Man', squad: 'Avengers' },
+  { id: 3, name: 'Spiderman', squad: 'Avengers' },
+  { id: 4, name: 'Superman', squad: 'Justice League' },
+  { id: 5, name: 'Wonder Woman', squad: 'Justice League' },
+  { id: 6, name: 'Aquaman', squad: 'Justice League' },
+  { id: 7, name: 'Hulk', squad: 'Avengers' },
+];
+
+function isEqualID(element){
+  return element.id === query.id;
+}
+
+function findOne(arr, query){
+  let queryValues = Object.values(query);
+  arr.find()
+}
