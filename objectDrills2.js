b6bc05c9fe8fbead50d1768d271469208da645ec
Nick Justin <Nicholasgjustin@gmail.com>
2018-12-05 11:22:31 -0600

    Added a function: enrollInSummerSchool which changes students statuses to In Summer School and nothing else.

--- objectDrills2.js
+++ objectDrills2.js
@@ -1,4 +1,4 @@
-function makeStudentsReport(data) {
+/*function makeStudentsReport(data) {
     let array = [];
     for (let i=0; i < data.length; i++) {
         data[i];
@@ -6,4 +6,86 @@ function makeStudentsReport(data) {
     }
     return array;
 
+  } */
+
+  const studentData = [
+    {
+      name: 'Tim',
+      status: 'Current student',
+      course: 'Biology',
+    },
+    {
+      name: 'Sue',
+      status: 'Withdrawn',
+      course: 'Mathematics',
+    },
+    {
+      name: 'Liz',
+      status: 'On leave',
+      course: 'Computer science',
+    },
+  ];
+  
+  function enrollInSummerSchool(students) {
+    for(x = 0; x < students.length; x++){
+        students[x].status = 'In Summer School';
+    }
+    return students;
   }
+  
+  /* From here down, you are not expected to understand.... for now :)
+    Nothing to see here!
+  
+  */
+  
+  // tests
+  
+  function testIt() {
+    var testData = [
+      {
+        name: 'Burt',
+        status: 'Playing hooky',
+        course: 'Biology',
+      },
+      {
+        name: 'Melanie',
+        status: 'Sick',
+        course: 'Mathematics',
+      },
+      {
+        name: 'Leonard',
+        status: 'AWOL',
+        course: 'Computer science',
+      },
+    ];
+  
+    var results = enrollInSummerSchool(testData);
+  
+    if (!(results && results instanceof Array)) {
+      console.error('FAILURE: `enrollSummerSchool` must return an array');
+      return;
+    }
+    var result = testData.every(function(student) {
+      var match = results.find(function(_student) {
+        return (
+          _student.name === student.name &&
+          _student.course === student.course &&
+          _student.status.toLowerCase() === 'in summer school'
+        );
+      });
+      return match !== undefined;
+    });
+    if (!result) {
+      console.error(
+        'FAILURE: `enrollSummerSchool` should return ' +
+          'original key/value pairs for each student, and ' +
+          'update `status` to "In Summer school": ' +
+          JSON.stringify(results)
+      );
+    } else {
+      console.info('SUCCESS: `enrollSummerSchool` is working');
+    }
+  }
+  
+  testIt();
+  
